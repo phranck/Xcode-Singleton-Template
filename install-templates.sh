4b3e403b6a1403727b9a29695a0494c80ea98a35
Frank Gregor <phranck@cocoanaut.com>
2015-10-20 20:54:27 +0200

    • changes install script • added .gitignore • removed .DS_Store

--- install-templates.sh
+++ install-templates.sh
@@ -3,7 +3,7 @@
 SOURCE_DIR=$(dirname "$0")
 TEMPLATES_DIR="$HOME/Library/Developer/Xcode/Templates"
 FILE_TEMPLATES_DIR="$TEMPLATES_DIR/File Templates"
-SINGLETON_TEMPLATES_DIR="$FILE_TEMPLATES_DIR/Source"
+SINGLETON_TEMPLATES_DIR="$FILE_TEMPLATES_DIR/Singleton"
 SOURCE_SINGLETON_DIR="$SOURCE_DIR/File Templates/Singleton"
 SOURCE_DIR="$SOURCE_SINGLETON_DIR/Objective-C Singleton class.xctemplate"
 
